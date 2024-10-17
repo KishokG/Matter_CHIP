011e97d3f9c4d53ec5d5eb6da29c9404071caebe
Kishok G <133193761+KishokG@users.noreply.github.com>
2024-10-17 23:52:49 +0530

    Update All_Issues_Pulling.py

--- Matter_Issues_Pull/All_Issues_Pulling.py
+++ Matter_Issues_Pull/All_Issues_Pulling.py
@@ -48,7 +48,7 @@ def fetch_github_issues(repo_name):
     while True:
         url = f"https://api.github.com/repos/{repo_name}/issues"
         headers = {
-            "Authorization": f"token {GITHUB_TOKEN}"
+            "Authorization": f"token {github_token}"
         }
         params = {
             "state": "open",  # Fetch only open issues (optional, remove for all issues)
